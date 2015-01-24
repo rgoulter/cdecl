504be77c3fdc689310afaf1d389f9e6c906aa63f
Richard Goulter <richard.goulter@gmail.com>
2015-01-24 18:15:52 +0800

    Add some test examples to JUnit.

---
+++ src/test/java/ExplainTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ExplainTest {
+	
+	public void assertGibberishToEnglish(String gibberish, String expectedEnglish) {
+		String actualEnglish = Explain.gibberishToEnglish(gibberish);
+		assertEquals(expectedEnglish, actualEnglish);
+	}
+
+	@Test
+	public void test() {
+		assertGibberishToEnglish("int x = 3;",
+		                         "x is int");
+
+		assertGibberishToEnglish("int x[4];",
+		                         "x is array 4 of int");
+
+		assertGibberishToEnglish("int *x;",
+		                         "x is pointer to int");
+
+		assertGibberishToEnglish("int *x[3];",
+		                         "x is array 3 of pointer to int");
+
+		assertGibberishToEnglish("int (*x[5])[3];",
+		                         "x is array 5 of pointer to array 3 of int");
+
+		assertGibberishToEnglish("int x[5][3];",
+		                         "x is array 5 of array 3 of int");
+
+		assertGibberishToEnglish("int *(*x)[3];",
+		                         "x is pointer to array 3 of pointer to int");
+
+		assertGibberishToEnglish("int (**x)[3];",
+		                         "x is pointer to pointer to array 3 of int");
+
+		assertGibberishToEnglish("int **x[3];",
+		                         "x is array 3 of pointer to pointer to int");
+
+		assertGibberishToEnglish("int (*x)(void);",
+		                         "x is pointer to function (void) returning int");
+
+	}
+
+}
